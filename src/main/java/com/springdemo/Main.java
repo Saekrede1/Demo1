b7a58e35a632eb52ee7ff98522c12b3aa5c37c68
Sai.Billa <bismyid@gmail.com>
2023-01-19 11:54:37 +0530

    pushing all the components to the repo

---
+++ src/main/java/com/springdemo/Main.java
@@ -0,0 +1,7 @@
+package com.springdemo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Java!");
+    }
+}
---
+++ src/test/java/com/springdemo/MainTest.java
@@ -0,0 +1,19 @@
+package com.springdemo;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Test
+    public void testCase1() {
+        System.setOut(new PrintStream(outContent));
+        Main.main(null);
+        assertEquals("Welcome to Java!", outContent.toString());
+        System.setOut(originalOut);
+    }
+}
